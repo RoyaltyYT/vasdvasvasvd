93b35acfde0b9d49f008329f00da20fac0ec38f8
RoyaltyYT <agarbot9@gmail.com>
2018-03-22 13:08:44 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-var server = app.listen(80);
-
-app.use(express.static('public', {
-	extensions: ['html', 'htm']
-}));
-console.log('Zoyi Website Started');
